dbcf8e1dc4cd6ba48daa396549e113061285d25a
ghazihussain <ghazihussainfazal@gmail.com>
2020-06-20 22:06:40 +0500

    Spinner color changes

--- src/utils/Spinner.js
+++ src/utils/Spinner.js
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import _Spinner from 'react-native-loading-spinner-overlay';
+import theme from './theme';
 
-const Spinner = ({visible}) => <_Spinner visible={visible} itemProp='size:100' color={'#A040A0'}/>;
+const Spinner = ({visible}) => <_Spinner visible={visible} itemProp='size:100' color={theme.colors.blue}/>;
 
 module.exports = {Spinner};
